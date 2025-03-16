e13c854dd64ce34290226c8684082aa428e955a2
Michael Kobets <michael.kobetss@gmail.com>
2025-03-16 20:43:31 +0200

    0.0.4

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// main.js
 document.addEventListener('DOMContentLoaded', () => {
     const app = document.getElementById('app');
 
@@ -10,9 +9,32 @@ document.addEventListener('DOMContentLoaded', () => {
     })
     .then(response => response.text())
     .then(bannerHtml => {
-        // Insert banner immediately after the header
         const headerElement = app.firstElementChild;
         headerElement.insertAdjacentHTML('afterend', bannerHtml);
+        return fetch('components/features.html');
+    })
+    .then(response => response.text())
+    .then(featuresHtml => {
+        const bannerElement = document.querySelector('.banner');
+        bannerElement.insertAdjacentHTML('afterend', featuresHtml);
+        return fetch('components/how-to-use.html');
+    })
+    .then(response => response.text())
+    .then(howToUseHtml => {
+        const featuresElement = document.querySelector('.features');
+        featuresElement.insertAdjacentHTML('afterend', howToUseHtml);
+        return fetch('components/tariffs.html');
+    })
+    .then(response => response.text())
+    .then(tariffsHtml => {
+        const howToUseElement = document.querySelector('.how-to-use');
+        howToUseElement.insertAdjacentHTML('afterend', tariffsHtml);
+        return fetch('components/contact.html'); // Добавляем загрузку contact.html
+    })
+    .then(response => response.text())
+    .then(contactHtml => {
+        const tariffsElement = document.querySelector('.tariffs'); // Опора на tariffs
+        tariffsElement.insertAdjacentHTML('afterend', contactHtml); // Вставляем contact.html
         return fetch('components/footer.html');
     })
     .then(response => response.text())
